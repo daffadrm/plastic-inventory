e5680752fb7b8ba093a74be3fb983a83c8609d55
daffadrm <daffarayhan18@gmail.com>
2025-08-03 00:49:10 +0700

    fix(dashboard): integration with api

--- src/app/server/dashboard/index.ts
+++ src/app/server/dashboard/index.ts
@@ -1,7 +1,12 @@
-import { localApiService } from '../localApiService'
+import { apiService, getHeader } from '../apiService'
+
+// import { localApiService } from '../localApiService'
+
+const versioning1 = process.env.NEXT_PUBLIC_API_V1
 
 export const getDashboard = async () => {
-  const response = await localApiService('/dummyDashboard.json', 'GET', null, () => false)
+  // const response = await localApiService('/dummyDashboard.json', 'GET', null, () => false)
+  const response = apiService(`${versioning1}/dashboard`, 'GET', null, getHeader())
 
   return response
 }
